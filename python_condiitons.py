0d027665b1ad7f28d57fb8ed29ef4f449e58b15d
Fiodor-Culacovschi <terany90@gmail.com>
2026-08-28 19:52:25 -0400

    Add Python another practice terminal + python exercise

---
+++ python_condiitons.py
@@ -0,0 +1,16 @@
+age = 20
+
+if age >= 18:
+	print("You are an adult")
+
+temp = 90
+
+if temp > 80:
+	print("It is too hot")
+
+my_age = 16
+
+if my_age >= 18:
+	print("Adult")
+else:
+   print("Minor")
