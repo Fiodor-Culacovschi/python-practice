032da0edfaad6654854b5ea7d23a052adeb07153
Fiodor-Culacovschi <terany90@gmail.com>
2026-08-28 19:45:31 -0400

    Played with datatypes and conditions for practice

---
+++ projects/data_types_conditions.py
@@ -0,0 +1,27 @@
+# creating variables for practice exercise 31 Git + Python Challenge
+age = 21
+temperature = 87.5
+name = "Sarah"
+is_student = True
+middle_name = None
+grocery = ["tea","lemon", "cookies","sugar"]
+weekdays = ("Monday","Tuesday","Wednesday","Thursday","Friday")
+student = {
+"name": "Alex",
+"age": 33,
+"city": "Miami",
+"is_employed": False
+}
+colors = {"red","blue","white","black","green"}
+
+if temperature >= 90:
+	print("Hot")
+else:
+    print("Cold")
+
+
+if "red" in colors:
+	print("Color found in the list)
+else:
+   print("Color not found")
+
